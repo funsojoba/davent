c6a71a4845c6a91312ddca060a0bc751c08a4d3d
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2022-02-04 15:58:46 +0100

    auth app setup

---
+++ authentication/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+    def create_user(self, email, password=None, **extrafields):
+        extrafields.setdefault("is_superuser", False)
+        return self._create_user(email=email, password=password, **extrafields)
+
+    def create_superuser(self, email, password=None, **extrafields):
+        extrafields.setdefault("is_superuser", True)
+        extrafields.setdefault("is_active", True)
+        extrafields.setdefault("is_staff", True)
+        return self._create_user(email=email, password=password, **extrafields)
+
+
+class User(AbstractBaseUser):
+    pass
