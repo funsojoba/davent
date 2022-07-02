e6aa233288e17d1e0376ddfade920ae6e5fca601
Funso Joba <hrfunsojoba@gmail.com>
2022-07-02 23:17:01 +0100

    added some migrations

--- event/urls.py
+++ event/urls.py
@@ -6,6 +6,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter(trailing_slash=False)
 
 router.register("user", UserEventViewSet, basename="event_user")
-router.register("", AdminEventViewSet, basename="event")
+router.register("admin", AdminEventViewSet, basename="event_admin")
 router.register(r"category", EventCategoryViewSet, basename="event-category")
 urlpatterns = router.urls
