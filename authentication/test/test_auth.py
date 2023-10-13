43e9b8e7febdb3cf471071493d4d199a417cd787
FunsoJoba <hrfunsojoba@gmail.com>
2023-10-13 20:10:54 +0100

    github action

--- authentication/test/test_auth.py
+++ authentication/test/test_auth.py
@@ -4,28 +4,28 @@ from rest_framework import status
 
 
 @pytest.mark.django_db
-def test_user_signup(client, user):
-    url = reverse("auth:signup")
-    data = {
-        "email": "new_user@example.com",
-        "password": "testpassword",
-        "first_name": "New",
-        "last_name": "User",
-        "phone_number": "1234567890",
-        "city": "Test City",
-        "state": "Test State",
-        "country": "Test Country",
-    }
-    response = client.post(url, data)
-    assert response.status_code == status.HTTP_201_CREATED
+class TestAuthentication:
+    def test_user_signup(client, user):
+        url = reverse("auth:signup")
+        data = {
+            "email": "new_user@example.com",
+            "password": "testpassword",
+            "first_name": "New",
+            "last_name": "User",
+            "phone_number": "1234567890",
+            "city": "Test City",
+            "state": "Test State",
+            "country": "Test Country",
+        }
+        response = client.post(url, data)
+        assert response.status_code == status.HTTP_201_CREATED
 
 
-@pytest.mark.django_db
-def test_user_login(client, user):
-    url = reverse("auth:login")
-    data = {
-        "email": user.email,
-        "password": "testpassword",
-    }
-    response = client.post(url, data)
-    assert response.status_code == status.HTTP_200_OK
+    def test_user_login(client, user):
+        url = reverse("auth:login")
+        data = {
+            "email": user.email,
+            "password": "testpassword",
+        }
+        response = client.post(url, data)
+        assert response.status_code == status.HTTP_200_OK
